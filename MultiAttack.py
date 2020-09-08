2f004f6235b4c7b2d1bb85095020219eb222dd38
blamedcloud <denverwoodward@yahoo.com>
2020-09-08 18:18:54 -0400

    crit bonus attack
    makes changes to the MultiAttack class for situations where you
    get an extra attack when you crit on an attack (For example, the
    first bullet point in the Great Weapon Master feat).
    
    Also fixes a small issue where the first hit damage wasn't added
    in the case where all the original attacks missed but the bonus
    attack on miss hit (or crit).
    
    adds another test where a build includes options for an extra miss
    attack, an extra crit attack, and first hit damage: test8.

--- MultiAttack.py
+++ MultiAttack.py
@@ -17,6 +17,9 @@ class MultiAttack(object):
         self.miss_atk = None
         self.miss_atk_ub = None
 
+        self.crit_atk = None
+        self.crit_atk_ub = None
+
         self.debug = False
 
     def copy(self):
@@ -50,12 +53,20 @@ class MultiAttack(object):
         self.miss_atk = atk
         _, self.miss_atk_ub = atk.get_bounds()
 
+    def add_crit_extra_attack(self, atk):
+        atk.finish_setup()
+        self.crit_atk = atk
+        _, self.crit_atk_ub = atk.get_bounds()
+
     def add_first_hit_damage(self, damage):
         self.first_hit_damage.add_damage(damage)
 
+    def has_bonus_atk(self):
+        return self.miss_atk is not None or self.crit_atk is not None
+
     def get_dmg_rv(self):
         all_dmg_rv = Constant(0)
-        if self.first_hit_damage.has_damage() or self.miss_atk is not None:
+        if self.first_hit_damage.has_damage() or self.has_bonus_atk():
             all_outcomes = self.generate_outcomes_product_()
             outcomes_rvs = {}
             outcomes_coeffs = {}
@@ -65,14 +76,7 @@ class MultiAttack(object):
                 if self.debug:
                     print("outcome number:",i)
                 outcome_coeff = self.get_attack_outcome_chance_(outcomes)
-
                 attacks_rv = self.get_attack_outcome_rvs_(outcomes)
-                fh_outcome = self.pick_first_passing_outcome_(outcomes, self.is_not_miss_)
-                if fh_outcome is None:
-                    fh_outcome = HitOutcome.MISS
-                fhd_dmg_rv = self.get_fhd_dmg_rv_(fh_outcome)
-                atks_and_fhd_rv = attacks_rv.add_rv(fhd_dmg_rv)
-                atks_and_fhd_rv.memoize()
 
                 fm_outcome = self.pick_first_passing_outcome_(outcomes, self.is_miss_)
                 if fm_outcome is not None and self.miss_atk is not None:
@@ -80,13 +84,53 @@ class MultiAttack(object):
                     for outcome in miss_atk_outcomes:
                         new_outcomes = outcomes + (outcome,)
                         new_coeff = outcome_coeff * self.miss_atk.get_outcome_chance(outcome)
-                        new_rv = atks_and_fhd_rv.add_rv(self.miss_atk.get_outcome_rv(outcome))
+                        new_rv = attacks_rv.add_rv(self.miss_atk.get_outcome_rv(outcome))
                         new_rv.memoize()
-                        outcomes_coeffs[new_outcomes] = new_coeff
-                        outcomes_rvs[new_outcomes] = new_rv
+
+                        fc_outcome = self.pick_first_passing_outcome_(new_outcomes, self.is_crit_)
+                        if fc_outcome is not None and self.crit_atk is not None:
+                            crit_atk_outcomes = self.crit_atk.get_outcomes()
+                            for outcome_c in crit_atk_outcomes:
+                                new_c_outcomes = new_outcomes + (outcome_c,)
+                                new_c_coeff = new_coeff * self.crit_atk.get_outcome_chance(outcome_c)
+                                new_c_rv = new_rv.add_rv(self.crit_atk.get_outcome_rv(outcome_c))
+                                new_c_rv.memoize()
+                                new_c_rv = self.handle_fh_outcome_(new_c_outcomes, new_c_rv)
+                                outcomes_coeffs[new_c_outcomes] = new_c_coeff
+                                outcomes_rvs[new_c_outcomes] = new_c_rv
+                        else:
+                            new_rv = self.handle_fh_outcome_(new_outcomes, new_rv)
+                            outcomes_coeffs[new_outcomes] = new_coeff
+                            outcomes_rvs[new_outcomes] = new_rv
                 else:
-                    outcomes_coeffs[outcomes] = outcome_coeff
-                    outcomes_rvs[outcomes] = atks_and_fhd_rv
+                    fc_outcome = self.pick_first_passing_outcome_(outcomes, self.is_crit_)
+                    if fc_outcome is not None and self.crit_atk is not None:
+                        crit_atk_outcomes = self.crit_atk.get_outcomes()
+                        for outcome in crit_atk_outcomes:
+                            new_outcomes = outcomes + (outcome,)
+                            new_coeff = outcome_coeff * self.crit_atk.get_outcome_chance(outcome)
+                            new_rv = attacks_rv.add_rv(self.crit_atk.get_outcome_rv(outcome))
+                            new_rv.memoize()
+
+                            fm_outcome = self.pick_first_passing_outcome_(new_outcomes, self.is_miss_)
+                            if fm_outcome is not None and self.miss_atk is not None:
+                                miss_atk_outcomes = self.miss_atk.get_outcomes()
+                                for outcome_m in miss_atk_outcomes:
+                                    new_m_outcomes = new_outcomes + (outcome_m,)
+                                    new_m_coeff = new_coeff * self.miss_atk.get_outcome_chance(outcome_m)
+                                    new_m_rv = new_rv.add_rv(self.miss_atk.get_outcome_rv(outcome_m))
+                                    new_m_rv.memoize()
+                                    new_m_rv = self.handle_fh_outcome_(new_m_outcomes, new_m_rv)
+                                    outcomes_coeffs[new_m_outcomes] = new_m_coeff
+                                    outcomes_rvs[new_m_outcomes] = new_m_rv
+                            else:
+                                new_rv = self.handle_fh_outcome_(new_outcomes, new_rv)
+                                outcomes_coeffs[new_outcomes] = new_coeff
+                                outcomes_rvs[new_outcomes] = new_rv
+                    else:
+                        attacks_rv = self.handle_fh_outcome_(outcomes, attacks_rv)
+                        outcomes_coeffs[outcomes] = outcome_coeff
+                        outcomes_rvs[outcomes] = attacks_rv
             def overall_pdf(x):
                 value = 0
                 for outcomes, coeff in outcomes_coeffs.items():
@@ -96,6 +140,8 @@ class MultiAttack(object):
             bonus_max = self.get_fhd_dmg_rv_(HitOutcome.CRIT).get_ub()
             if self.miss_atk_ub is not None:
                 bonus_max += self.miss_atk_ub - self.damage_min_ub
+            if self.crit_atk_ub is not None:
+                bonus_max += self.crit_atk_ub
             all_dmg_rv = RandomVariable(0,self.damage_max + bonus_max)
             all_dmg_rv.set_pdf(overall_pdf)
         else:
@@ -106,6 +152,14 @@ class MultiAttack(object):
                 all_dmg_rv.memoize()
         all_dmg_rv.memoize()
         return all_dmg_rv
+
+    def handle_fh_outcome_(self, outcomes, input_rv):
+        fh_outcome = self.pick_first_passing_outcome_(outcomes, self.is_not_miss_)
+        if fh_outcome is not None:
+            fh_rv = self.get_fhd_dmg_rv_(fh_outcome)
+            input_rv = input_rv.add_rv(fh_rv)
+            input_rv.memoize()
+        return input_rv
 
     def get_attack_outcome_chance_(self, outcomes):
         product = 1
@@ -133,6 +187,9 @@ class MultiAttack(object):
 
     def is_miss_(self, outcome):
         return outcome == HitOutcome.MISS
+
+    def is_crit_(self, outcome):
+        return outcome == HitOutcome.CRIT
 
     def pick_first_passing_outcome_(self, outcomes, criteria):
         for i in range(len(outcomes)):
---
+++ test8.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+from MultiAttack import *
+from Attack import *
+from Damage import *
+
+if __name__ == "__main__":
+
+    # enemy assumptions
+    armor_class = 17
+    hit_type = HitType.NORMAL
+    resisted = False
+    auto_crit = False
+
+    turn_one = True
+
+    # lvl 12 gloomstalker ranger, rogue 1 using rapier, 20 dex, sneak attack
+    # and Great Weapon Master (solely for the bonus crit atk opportunity)
+    # this is obviously not optimal, but is a useful scenario for testing
+    # (also has dueling fighting style)
+    damage = Damage("1d8 + 5 + 2", resisted)
+    bonus_atk_damage = Damage("2d8 + 5 + 2", resisted)
+    sneak_atk_dmg = Damage("1d6", resisted)
+
+    hit_bonus = Constant(10) # dex + prof = 5 + 5
+
+    attack = Attack(hit_bonus, armor_class, hit_type, auto_crit = auto_crit)
+    attack.add_damage(damage)
+
+    attack2 = attack.copy()
+
+    attack3 = Attack(hit_bonus, armor_class, hit_type, auto_crit = auto_crit)
+    attack3.add_damage(bonus_atk_damage)
+
+    attack_miss = attack.copy()
+
+    attack_crit = attack.copy()
+
+    round_dmg = MultiAttack()
+    round_dmg.add_attack(attack)
+    round_dmg.add_attack(attack2)
+
+    if turn_one:
+        round_dmg.add_attack(attack3)
+
+    round_dmg.add_miss_extra_attack(attack_miss)
+
+    round_dmg.add_first_hit_damage(sneak_atk_dmg)
+
+    round_dmg.add_crit_extra_attack(attack_crit)
+
+    dpr = round_dmg.get_dmg_rv()
+
+    dpr.describe(True)
+
