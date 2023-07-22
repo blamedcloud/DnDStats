246a00a3fede4e7f485fbe86f1a3a5e6e8c04158
blamedcloud <denverwoodward@yahoo.com>
2023-07-21 20:51:45 -0400

    clean up random variable code a bit

--- rand_var/src/lib.rs
+++ rand_var/src/lib.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
-use num::{FromPrimitive, Num, PrimInt};
+use num::{FromPrimitive, Integer, Num, One, PrimInt};
+use num::rational::Ratio;
 
 pub struct RandomVariable<T: Num> {
     lower_bound: isize,
@@ -7,13 +8,71 @@ pub struct RandomVariable<T: Num> {
     pdf_vec: Vec<T>,
 }
 
-impl<T: Num + Debug> RandomVariable<T> {
+impl<T> RandomVariable<Ratio<T>>
+where
+    T: Integer + Debug + Clone + FromPrimitive
+{
+    pub fn new_dice(sides: isize) -> Self {
+        assert!(sides > 0, "sides must be positive");
+        <RandomVariable<Ratio<T>> as RandVar<isize, Ratio<T>>>::build(
+            1,
+            sides,
+            |_| {
+                Ratio::new(T::one(), T::from_isize(sides).unwrap())
+            })
+    }
 
-    pub fn build<F>(lb: isize, ub: isize, f: F) -> RandomVariable<T>
+    pub fn new_dice_reroll(sides: isize, reroll_max: isize) -> Self {
+        assert!(sides > 0, "sides must be positive");
+        assert!(reroll_max > 0, "reroll_max must be positive");
+        assert!(sides > reroll_max, "sides must be larger than reroll_max");
+        let one_over_sides = Ratio::new(
+            T::one(),
+            T::from_isize(sides).unwrap());
+        let reroll_over_sides = Ratio::new(
+            T::from_isize(reroll_max).unwrap(),
+            T::from_isize(sides).unwrap());
+        let reroll_only = reroll_over_sides * one_over_sides.clone();
+        let possible_reroll = one_over_sides + reroll_only.clone();
+        <RandomVariable<Ratio<T>> as RandVar<isize, Ratio<T>>>::build(
+            1,
+            sides,
+            |x| {
+                if x > reroll_max {
+                    possible_reroll.clone()
+                } else {
+                    reroll_only.clone()
+                }
+            })
+    }
+
+    pub fn new_constant(value: isize) -> Self {
+        <RandomVariable<Ratio<T>> as RandVar<isize, Ratio<T>>>::build(
+            value,
+            value,
+            |_| Ratio::one())
+    }
+
+    pub fn new_uniform(lb: isize, ub: isize) -> Self {
+        <RandomVariable<Ratio<T>> as RandVar<isize, Ratio<T>>>::build(
+            lb,
+            ub,
+            |_| {
+                Ratio::new(T::one(), T::from_isize(ub-lb+1).unwrap())
+            })
+    }
+
+}
+
+impl<T> RandVar<isize, T> for RandomVariable<T>
+where
+    T: Num + Debug + Clone
+{
+    fn build<F>(lb: isize, ub: isize, f: F) -> RandomVariable<T>
     where
         F: Fn(isize) -> T
     {
-        assert!(lb <= ub);
+        assert!(lb <= ub, "lower bound must be <= upper bound");
         let length: usize = (ub - lb + 1) as usize;
         let mut pdf_vec = Vec::with_capacity(length);
         let mut total = T::zero();
@@ -21,7 +80,7 @@ impl<T: Num + Debug> RandomVariable<T> {
             total = total + f(i);
             pdf_vec.push(f(i));
         }
-        assert_eq!(T::one(), total);
+        assert_eq!(T::one(), total, "cdf(upper bound) must be 1");
 
         RandomVariable {
             lower_bound: lb,
@@ -29,12 +88,7 @@ impl<T: Num + Debug> RandomVariable<T> {
             pdf_vec
         }
     }
-}
 
-impl<T> RandVar<isize, T> for RandomVariable<T>
-where
-    T: Num + Debug + Clone
-{
     fn lower_bound(&self) -> isize {
         self.lower_bound
     }
@@ -52,8 +106,8 @@ where
 }
 
 impl<T> NumRandVar<isize, T> for RandomVariable<T>
-    where
-        T: Num + Debug + Clone + FromPrimitive
+where
+    T: Num + Debug + Clone + FromPrimitive
 {
     fn convert(&self, p: isize) -> T {
         T::from_isize(p).unwrap()
@@ -66,10 +120,11 @@ where
     P: Ord,
     T: Num,
 {
+    fn build<F: Fn(P) -> T>(lb: P, ub: P, f: F) -> Self;
     fn lower_bound(&self) -> P;
     fn upper_bound(&self) -> P;
-
     fn raw_pdf(&self, p: P) -> T;
+
     fn pdf(&self, p: P) -> T {
         if (self.lower_bound() <= p) && (p <= self.upper_bound()) {
             self.raw_pdf(p)
@@ -80,10 +135,10 @@ where
 }
 
 fn summation<P, T, F>(lb: P, ub: P, f: F) -> T
-    where
-        P: PrimInt,
-        T: Num,
-        F: Fn(P) -> T,
+where
+    P: PrimInt,
+    T: Num,
+    F: Fn(P) -> T,
 {
     let mut result = T::zero();
     for p in num::range_inclusive(lb, ub) {
@@ -93,18 +148,18 @@ fn summation<P, T, F>(lb: P, ub: P, f: F) -> T
 }
 
 fn convolution<P, T, F>(lb: P, ub: P, f1: F, f2: F, x: P) -> T
-    where
-        P: PrimInt,
-        T: Num,
-        F: Fn(P) -> T,
+where
+    P: PrimInt,
+    T: Num,
+    F: Fn(P) -> T,
 {
     summation(lb, ub, |y| f1(x-y)*f2(y))
 }
 
 pub trait NumRandVar<P, T>: RandVar<P, T>
-    where
-        P: PrimInt,
-        T: Num + Clone,
+where
+    P: PrimInt,
+    T: Num + Clone,
 {
     fn cdf(&self, p: P) -> T {
         if self.upper_bound() <= p {
@@ -123,8 +178,8 @@ pub trait NumRandVar<P, T>: RandVar<P, T>
     }
 
     fn general_expected_value<F>(&self, f: F) -> T
-        where
-            F: Fn(P) -> T
+    where
+        F: Fn(P) -> T
     {
         let mut result = T::zero();
         for p in num::range_inclusive(self.lower_bound(), self.upper_bound()) {
--- rand_var/src/main.rs
+++ rand_var/src/main.rs
@@ -2,11 +2,13 @@ use num::rational::Rational64;
 use rand_var::{NumRandVar, RandomVariable};
 
 fn main() {
-    let one_sixth = Rational64::new(1,6);
-    let rv = RandomVariable::build(1,6,|_x| {one_sixth});
+    let rv: RandomVariable<Rational64> = RandomVariable::new_dice(6);
     let cdf3 = rv.cdf(3);
     println!("cdf(3): {}", cdf3);
 
     let ev = rv.expected_value();
     println!("EV[X] = {}", ev);
+
+    let d10r2: RandomVariable<Rational64> = RandomVariable::new_dice_reroll(10,2);
+    println!("EV[d10r2] = {}", d10r2.expected_value());
 }
