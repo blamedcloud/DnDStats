65e1e5a3bb372a15defdf2828cdcbf53a968d4a1
blamedcloud <denverwoodward@yahoo.com>
2023-07-23 20:20:57 -0400

    reorganized code into module

--- rand_var/src/main.rs
+++ rand_var/src/main.rs
@@ -1,5 +1,7 @@
 use num::rational::Rational64;
-use rand_var::{NumRandVar, RandomVariable, RandVar};
+use num::ToPrimitive;
+use rand_var::RandomVariable;
+use rand_var::rv_traits::{NumRandVar, RandVar};
 
 fn main() {
     let rv: RandomVariable<Rational64> = RandomVariable::new_dice(6);
@@ -11,4 +13,7 @@ fn main() {
 
     let d10r2: RandomVariable<Rational64> = RandomVariable::new_dice_reroll(10,2);
     println!("EV[d10r2] = {}", d10r2.expected_value());
+
+    d10r2.print_pdf(|x| x.to_f64().unwrap());
+    d10r2.print_stats_convert(|x| x.to_f64().unwrap());
 }
